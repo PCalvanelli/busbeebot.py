f5ded0d4be940cc3190f476939233ceef5f84a2e
PCalvanelli <peter.j.calvanelli@gmail.com>
2020-06-17 17:36:06 -0400

    v1 of busbeebot
    MVP of busbee bot

---
+++ first_app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+from PIL import Image
+
+#header and version title
+st.write('v',1.0)
+st.title('Busbee Bot :palm_tree: :v:')
+
+#Shitball Avatar
+
+image = Image.open('john.jpg')
+st.image(image, caption="Hello! I am your new data analyst, Shitball Higgins!", width=200)
+
+#Upload CSV File
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+#Upload CSV File through widget
+#Add spinner to simulate shitball Higgins reading data
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    with st.spinner(text='Please hold. Shitball Higgins is attempting to read your data...'):
+        time.sleep(1)
+    st.write(df)
+    st.success('Shitball Higgins :hankey: successfully loaded your file!')
+'''
+
+---
+
+'''
+
+#Select specific variables for data analysis
+#Pulls columns data as variables
+
+st.header("Filter Variables")
+#Select Columns to display
+
+if df is not None:
+    all_columns = df.columns.tolist()
+    selected_columns = st.multiselect("Select",all_columns)
+    new_df = df[selected_columns]
+    st.dataframe(new_df)
+
+#Show summary
+if st.button("Describe Data"):
+    with st.spinner(text='Please hold. Shitball Higgins :hankey: is looking intensly at your data...'):
+        time.sleep(3)
+    st.write(new_df.describe().T)
+    st.success("Shitball Higgins found some gems :gem:!")
+
+'''
+
+---
+
+'''
+
+#Plot and Visualize
+st.header("Sample Visualization :bar_chart:")
+st.write("Now that you have selected that variables you'd like to explore, let's look at one way this can be visualized.")
+if st.button("Show Plot"):
+    st.bar_chart(new_df)
